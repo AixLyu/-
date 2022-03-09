c896a37fe854b7804811a3631c29e8cf2db509bf
AixLyu <100019045+AixLyu@users.noreply.github.com>
2022-03-09 14:25:29 +0800

    Add files via upload

---
+++ Hello world!/Hello world!/01.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello world!\n");
+	printf("hello wangdao\n");
+	printf("Hello BUPT\n");
+	printf("Beijing University of Posts and Telecommunications\n");
+	return 0;
+}
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf_s("%d%d", &a, &b);
+//	int c = a + b;
+//	printf("%d", c);
+//    return 0;
+//}
---
+++ Hello world!/Hello world!/02.C源.cpp
@@ -0,0 +1,11 @@
+#define_CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = a + b;
+	scanf("%d%d",a,b)
+	printf("Hello world!");
+}
